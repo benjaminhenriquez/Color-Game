a45c73a4e92e3348b3194007bb217c854a07015c
benjaminhenriquez <henriquez.ben@gmail.com>
2017-06-04 19:56:46 -0400

    added js

---
+++ index.js
@@ -0,0 +1,14 @@
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(250, 125, 0)",
+  "rgb(250, 225, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 0, 255)",
+  "rgb(175, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+
+for(var i=0; i<squares.length; i++){
+  squares[i].style.backgrounndColor[i]
+}
