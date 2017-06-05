31dd784e3d20cc6985a348874d0a7d76b54ffa01
benjaminhenriquez <henriquez.ben@gmail.com>
2017-06-05 00:56:05 -0400

    colors

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ var colors = [
   "rgb(0, 255, 0)",
   "rgb(0, 0, 255)",
   "rgb(175, 0, 255)"
-]
+];
+
 
 var squares = document.querySelectorAll(".square");
 
-for(var i=0; i<squares.length; i++){
-  squares[i].style.backgrounndColor[i]
+var pickedColor = colors[1];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+  squares[i].style.backgroundColor = colors[i];
+  squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.backgroundColor;
+    if( clickedColor === pickedColor){
+      alert("Correct");
+    }
+    else {
+      this.style.backgroundColor = "#232323"
+    }
+  });
 }
