9728c399c72ec79c9afba9496ab8f43f7f67d0b1
benjaminhenriquez <henriquez.ben@gmail.com>
2017-06-06 15:33:02 -0400

    fixed RGB not showing bug, and had level buttons change color

--- index.js
+++ index.js
@@ -10,33 +10,44 @@ var colors = [
   "rgb(255, 255, 255)"
 ];
 
-
-
+var level = 6;
+document.querySelector(".on").style.backgroundColor = "rgb(150, 250, 200)";
+var newColors = generateRandomColors(level);
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
-var level = 6;
-var newColors = generateRandomColors(level);
 var header = document.querySelector("#header");
+var pickedColor;
 
 
-document.querySelector("#easy").addEventListener("click", changeLevel(3));
-document.querySelector("#medium").addEventListener("click", changeLevel(6));
-document.querySelector("#hard").addEventListener("click", changeLevel(9));
 
-colorDisplay.textContent = pickedColor;
+
+var easy = document.querySelector("#easy")
+  easy.addEventListener("click", function(){changeLevel(3)});
+var medium = document.querySelector("#medium")
+  medium.addEventListener("click", function(){changeLevel(6)});
+var hard = document.querySelector("#hard")
+  hard.addEventListener("click", function(){changeLevel(9)});
+
+
 
 assignColors();
 
+
+// functions below
+
+
 function assignColors(){
-  for(var i = 0; i < squares.length; i++){
+  pickedColor = pickColor()
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < level; i++){
     squares[i].style.backgroundColor = colors[i];
     squares[i].addEventListener("click", function(){
       var clickedColor = this.style.backgroundColor;
       if( clickedColor === pickedColor){
         messageDisplay.textContent = "Correct";
         changeColors(clickedColor);
+        document.querySelector(".on").style.backgroundColor = pickedColor
         header.style.backgroundColor = clickedColor;
         header.style.color = "rgb(50, 50, 50)";
         var rgb = document.querySelectorAll("#squares")
@@ -53,22 +64,33 @@ function assignColors(){
 }
 
 function changeLevel(num){
+  var container = document.querySelector("#container")
+  var html =  "<div class='square'></div><div class='square'></div><div class='square'></div>"
   if(num === 3){
     level = 3;
-    document.querySelector("#container").innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div>";
+    container.innerHTML = html;
     squares = document.querySelectorAll(".square");
+    medium.classList.remove("on")
+    hard.classList.remove("on")
+    easy.classList.add("on")
     assignColors();
   }
   else if (num === 6){
     level = 6;
-    document.querySelector("#container").innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div>";
+    container.innerHTML = html + html;
     squares = document.querySelectorAll(".square");
+    easy.classList.remove("on")
+    hard.classList.remove("on")
+    medium.classList.add("on")
     assignColors();
   }
   else if (num === 9) {
     level = 9;
-    document.querySelector("#container").innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div>";
+    container.innerHTML = html + html + html;
     squares = document.querySelectorAll(".square");
+    medium.classList.remove("on")
+    easy.classList.remove("on")
+    hard.classList.add("on")
     assignColors();
   }
 }
@@ -84,6 +106,10 @@ function changeColors(color){
     squares[i].style.backgroundColor = color;
   }
 }
+
+
+
+//not used yet
 
 function generateRandomColors(num){
   var arr = []
