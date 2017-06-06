8cf31325944bdd59bddb1ed0cc6dccef25a4ae46
benjaminhenriquez <henriquez.ben@gmail.com>
2017-06-06 03:33:37 -0400

    added third row

--- index.js
+++ index.js
@@ -1,52 +1,86 @@
 var colors = [
   "rgb(255, 0, 0)",
-  "rgb(250, 150, 0)",
-  "rgb(250, 250, 0)",
   "rgb(0, 255, 0)",
   "rgb(0, 0, 255)",
-  "rgb(125, 0, 255)"
+  "rgb(255, 150, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(150, 0, 255)",
+  "rgb(0, 255, 255)",
+  "rgb(255, 0, 255)",
+  "rgb(255, 255, 255)"
 ];
 
 
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor(colors);
-var colorDisplay = document.getElementById("colorDisplay");
+var pickedColor = pickColor();
+var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
-var newColors = generateRandomColors(6);
+var level = 6;
+var newColors = generateRandomColors(level);
 var header = document.querySelector("#header");
+
+
+document.querySelector("#easy").addEventListener("click", changeLevel(3));
+document.querySelector("#medium").addEventListener("click", changeLevel(6));
+document.querySelector("#hard").addEventListener("click", changeLevel(9));
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < squares.length; i++){
-  squares[i].style.backgroundColor = colors[i];
-  squares[i].addEventListener("click", function(){
-    var clickedColor = this.style.backgroundColor;
-    if( clickedColor === pickedColor){
-      messageDisplay.textContent = "Correct";
-      changeColors(clickedColor);
-      header.style.backgroundColor = clickedColor;
-      header.style.color = "rgb(50, 50, 50)";
-      var rgb = document.querySelectorAll("span")
-      for(var i = 0; i < rgb.length; i++){
-        rgb[i].style.color = "rgb(50, 50, 50)";
+assignColors();
+
+function assignColors(){
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].addEventListener("click", function(){
+      var clickedColor = this.style.backgroundColor;
+      if( clickedColor === pickedColor){
+        messageDisplay.textContent = "Correct";
+        changeColors(clickedColor);
+        header.style.backgroundColor = clickedColor;
+        header.style.color = "rgb(50, 50, 50)";
+        var rgb = document.querySelectorAll("#squares")
+        for(var i = 0; i < level; i++){
+          rgb[i].style.color = "rgb(50, 50, 50)";
+        }
       }
-    }
-    else {
-      this.style.backgroundColor = "rgb(50, 50, 50)";
-      messageDisplay.textContent = "Try Again";
-    }
-  });
+      else {
+        this.style.backgroundColor = "rgb(50, 50, 50)";
+        messageDisplay.textContent = "Try Again";
+      }
+    });
+  }
 }
 
-function pickColor(arr){
-  var random = Math.floor(Math.random() * colors.length);
-  return arr[random];
+function changeLevel(num){
+  if(num === 3){
+    level = 3;
+    document.querySelector("#container").innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div>";
+    squares = document.querySelectorAll(".square");
+    assignColors();
+  }
+  else if (num === 6){
+    level = 6;
+    document.querySelector("#container").innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div>";
+    squares = document.querySelectorAll(".square");
+    assignColors();
+  }
+  else if (num === 9) {
+    level = 9;
+    document.querySelector("#container").innerHTML = "<div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div>";
+    squares = document.querySelectorAll(".square");
+    assignColors();
+  }
+}
+
+function pickColor(){
+  var random = Math.floor(Math.random() * level);
+  return colors[random];
 }
 
 
 function changeColors(color){
-  for(var i = 0; i < squares.length; i++){
+  for(var i = 0; i < level; i++){
     squares[i].style.backgroundColor = color;
   }
 }
