b2b407d1fa886385768c8841f046af737fb3391b
benjaminhenriquez <henriquez.ben@gmail.com>
2017-06-10 20:53:47 -0400

    added random color generator

--- index.js
+++ index.js
@@ -10,31 +10,29 @@ var colors = [
   "rgb(255, 255, 255)"
 ];
 
+var black = "rgb(50, 50, 50)";
+var white = "rgb(250, 250, 250)";
 var level = 6;
 var newColors = generateRandomColors(level);
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var header = document.querySelector("#header");
+var reset = document.querySelector("#reset");
 var pickedColor;
-
-
-
-
 var easy = document.querySelector("#easy")
-  easy.addEventListener("click", function(){changeLevel(3)});
 var medium = document.querySelector("#medium")
-  medium.addEventListener("click", function(){changeLevel(6)});
 var hard = document.querySelector("#hard")
-  hard.addEventListener("click", function(){changeLevel(9)});
 
-
+easy.addEventListener("click", function(){changeLevel(3)});
+medium.addEventListener("click", function(){changeLevel(6)});
+hard.addEventListener("click", function(){changeLevel(9)});
+reset.addEventListener("click",function(){generateRandomColors()
+  assignColors();})
 
 assignColors();
 
-
 // functions below
-
 
 function assignColors(){
   pickedColor = pickColor()
@@ -44,21 +42,18 @@ function assignColors(){
     squares[i].addEventListener("click", function(){
       var clickedColor = this.style.backgroundColor;
       if( clickedColor === pickedColor){
+        reset.classList.remove("hidden")
         messageDisplay.textContent = "Correct";
         changeColors(clickedColor);
-        document.querySelector(".on").style.backgroundColor = pickedColor;
-        document.querySelector(".R").style.color = "rgb(50, 50, 50)";
-        document.querySelector(".G").style.color = "rgb(50, 50, 50)";
-        document.querySelector(".B").style.color = "rgb(50, 50, 50)";
         header.style.backgroundColor = clickedColor;
-        header.style.color = "rgb(50, 50, 50)";
+        header.style.color = black;
         var rgb = document.querySelectorAll("#squares")
         for(var i = 0; i < level; i++){
-          rgb[i].style.color = "rgb(50, 50, 50)";
+          rgb[i].style.color = black;
         }
       }
       else {
-        this.style.backgroundColor = "rgb(50, 50, 50)";
+        this.style.backgroundColor = black;
         messageDisplay.textContent = "Try Again";
       }
     });
@@ -67,14 +62,11 @@ function assignColors(){
 
 function changeLevel(num){
   function resetHeaderColor(){
-    document.querySelector("#header").style.backgroundColor = "rgb(50, 50, 50)"
-    document.querySelector("#header").style.color = "rgb(250, 250, 250)";
-    document.querySelector(".on").style.backgroundColor = "rgb(50, 50, 50)";
-    document.querySelectorAll(".off")[0].style.backgroundColor = "rgb(240, 240, 240)";
-    document.querySelectorAll(".off")[1].style.backgroundColor = "rgb(240, 240, 240)";
-    document.querySelector(".R").style.color = "rgb(255, 0, 0)";
-    document.querySelector(".G").style.color = "rgb(0, 255, 0)";
-    document.querySelector(".B").style.color = "rgb(0, 0, 255)";
+    document.querySelector("#header").style.backgroundColor = black
+    document.querySelector("#header").style.color = white;
+    // document.querySelector(".on").style.backgroundColor = black;
+    // document.querySelectorAll(".off")[0].style.backgroundColor = black;
+    // document.querySelectorAll(".off")[1].style.backgroundColor = black;
   }
   var container = document.querySelector("#container")
   var html =  "<div class='square'></div><div class='square'></div><div class='square'></div>"
@@ -82,12 +74,12 @@ function changeLevel(num){
     level = 3;
     container.innerHTML = html;
     squares = document.querySelectorAll(".square");
-    medium.classList.remove("on")
-    medium.classList.add("off")
-    hard.classList.remove("on")
-    hard.classList.add("off")
-    easy.classList.add("on")
-    easy.classList.remove("off")
+    // medium.classList.remove("on")
+    // medium.classList.add("off")
+    // hard.classList.remove("on")
+    // hard.classList.add("off")
+    // easy.classList.add("on")
+    // easy.classList.remove("off")
     assignColors();
     resetHeaderColor();
   }
@@ -95,12 +87,12 @@ function changeLevel(num){
     level = 6;
     container.innerHTML = html + html;
     squares = document.querySelectorAll(".square");
-    easy.classList.remove("on")
-    easy.classList.add("off")
-    hard.classList.remove("on")
-    hard.classList.add("off")
-    medium.classList.add("on")
-    medium.classList.remove("off")
+    // easy.classList.remove("on")
+    // easy.classList.add("off")
+    // hard.classList.remove("on")
+    // hard.classList.add("off")
+    // medium.classList.add("on")
+    // medium.classList.remove("off")
     assignColors();
     resetHeaderColor();
   }
@@ -108,12 +100,12 @@ function changeLevel(num){
     level = 9;
     container.innerHTML = html + html + html;
     squares = document.querySelectorAll(".square");
-    medium.classList.remove("on");
-    medium.classList.add("off");
-    easy.classList.remove("on");
-    easy.classList.add("off")
-    hard.classList.add("on");
-    hard.classList.remove("off")
+    // medium.classList.remove("on");
+    // medium.classList.add("off");
+    // easy.classList.remove("on");
+    // easy.classList.add("off")
+    // hard.classList.add("on");
+    // hard.classList.remove("off")
     assignColors();
     resetHeaderColor();
   }
@@ -136,9 +128,9 @@ function changeColors(color){
 //not used yet
 
 function generateRandomColors(num){
-  var arr = []
-  for(var i = 0; i < num; i++){
-    arr.push(randomColor())
+  colors = []
+  for(var i = 0; i < 9; i++){
+    colors.push(randomColor())
   }
 }
 
